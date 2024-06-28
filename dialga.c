1055ec833b64a2521d736611756f54fb15bd1b0b
Ayush (berzi) <123345456+Spirizeon@users.noreply.github.com>
2024-06-28 12:51:20 +0530

    Add files via upload

--- dialga.c
+++ dialga.c
@@ -12,7 +12,8 @@ void init(char* repo_name){
 		perror("mkdir");
 		return;
 	}
-	char temp_name = *repo_name;
+	char temp_name[MAX_BUF_SIZE];
+	strcpy(temp_name,repo_name);
 	strcat(temp_name,"/.dlgx");
 	if(mkdir(temp_name,0755) == 0){
 	}else{
@@ -21,20 +22,26 @@ void init(char* repo_name){
 	}
 	char* dlgx_contents[] = {"objects","refs","refs/heads"};
 	for(int i=0;i<3;i++){
-		temp_name = *repo_name;
-		strcat(&temp_name,"/");
-		strcat(&temp_name,dlgx_contents[i]);
+		strcpy(temp_name,repo_name);
+
+		strcat(temp_name,"/.dlgx/");
+		strcat(temp_name,dlgx_contents[i]);
+
 		if(mkdir(temp_name,0755) == 0){
 		}else{
 			perror("mkdir");
 			return;
 		}
 	}
-		printf("dialga (.dlgx) repo initialised!\n");
+		printf("\n===================================\n");
+		printf("dialga: your 💖 for git, debloated");
+		printf("\n===================================\n");
+		printf("repo %s initialised with (.dlgx)!\n\n",repo_name);
 }
 
 int main(){
 	char repo_name[MAX_BUF_SIZE];
+	printf("enter repo name: ");
 	scanf("%s",repo_name);
 	init(repo_name);
 	return 0;
