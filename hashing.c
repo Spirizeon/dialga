c351f2f08107b304b6b0a5f828239c9039a0fcbb
berzi <anon>
2024-06-30 16:21:01 +0530

    shifted back to SHA-1 for now

--- hashing.c
+++ hashing.c
@@ -4,11 +4,10 @@
 #include <string.h>
 #define MAX_BUF_SIZE 1024
 
-
 //function that returns the hash of a file 
 void hash_file(const char *repo_name,const char *file_name){
 		
-		char sha256_digest[SHA256_DIGEST_LENGTH];
+		char sha256_digest[SHA_DIGEST_LENGTH];
 		int bufsize = MAX_BUF_SIZE;
 		int position = 0;
 		char *buffer = malloc(sizeof(char) * bufsize);
@@ -38,7 +37,7 @@ void hash_file(const char *repo_name,const char *file_name){
 			position++;
 		} while(ch != EOF); 
 		
-			SHA256(buffer,strlen(buffer),sha256_digest);
+			SHA1(buffer,strlen(buffer),sha256_digest);
 	//time for hash -> hexadecimal
 	int hex_index = 0;
 	size_t hex_digest_len = strlen(sha256_digest) * 2 + 1;
@@ -76,8 +75,18 @@ void hash_file(const char *repo_name,const char *file_name){
 	else{
 		perror("dialga: could not use repoer");
 	}
+	
+	//time to create the blob file
+	FILE *blob;
+	strcat(repoer,"/");
+	strcat(repoer,hex_digest);
+	blob = fopen(repoer,"a");
+	
+	fclose(blob);
+	
 	free(buffer);
 	free(hex_digest);
+
 }
  
 
